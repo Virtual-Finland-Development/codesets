b07f9759bb9e68de517044d629febdde6715fc3b
Lassi Piironen <lsipii@kapsi.fi>
2023-03-02 12:40:23 +0200

    fix: provide local assets info to the live with json build file

--- src/scripts/prepare-dist.ts
+++ src/scripts/prepare-dist.ts
@@ -2,12 +2,5 @@ import * as fs from "fs";
 import { InternalResources } from "../resources/index";
 
 const internalResources = InternalResources.listResources(true);
-
-const internalsDistPath = `${process.cwd()}/dist/resources/internal`;
-if (!fs.existsSync(internalsDistPath)){
-    fs.mkdirSync(internalsDistPath, { recursive: true });
-}
-for (const resource of internalResources) {
-    const filepath = `${internalsDistPath}/${resource}`;
-    fs.closeSync(fs.openSync(filepath, 'w'));
-}
+const buildInternalResourcesPath = `${process.cwd()}/dist/resources/_build/internal-resources.json`;
+fs.writeFileSync(buildInternalResourcesPath, JSON.stringify(internalResources));
