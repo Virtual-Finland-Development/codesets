24cdaac6653a11c47bec59912145e260d19d3b5f
Lassi Piironen <lsipii@kapsi.fi>
2023-03-08 15:14:30 +0200

    fix: add countries transformer

--- src/resources/external/ISO3166CountriesURL.ts
+++ src/resources/external/ISO3166CountriesURL.ts
@@ -4,4 +4,18 @@ export default new ExternalResource({
     name: "ISO3166CountriesURL",
     uri: "https://github.com/mledoze/countries/blob/master/countries.json?raw=true",
     mime: "application/json; charset=utf-8",
+    async transformer(data: string) {
+        const countriesRaw = JSON.parse(data);
+        const countries = countriesRaw.map((country: any) => {
+            return {
+                "id": country.cca2,
+                "displayName": country.name.common,
+                "englishName": country.name.official,
+                "nativeName": "",
+                "twoLetterISORegionName": country.cca2,
+                "threeLetterISORegionName": country.cca3
+            };
+        });
+        return JSON.stringify(countries);
+    },
 });
