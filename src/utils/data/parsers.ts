43f8aac267e3b19f9f2405045d5a9efda9be3a23
Lassi Piironen <lsipii@kapsi.fi>
2023-03-10 18:44:29 +0200

    docs: add comments

--- src/utils/data/parsers.ts
+++ src/utils/data/parsers.ts
@@ -1,9 +1,15 @@
 import typia from 'typia';
 
+/**
+ * @returns function for: parse to specific type of object
+ */
 export function getInput() {
     return typia.assertParse;
 }
 
+/**
+ * @returns function for: parse and stringify
+ */
 export function getOutput() {
     return typia.assertStringify;
 }
