696e5893eee38783d40a5f0a451b768fba925081
Lassi Piironen <lsipii@kapsi.fi>
2023-03-13 10:47:33 +0200

    feat(build): on a deploy, create cache invalidation call on the /resources path

--- infra/index.ts
+++ infra/index.ts
@@ -1,8 +1,8 @@
-import * as pulumi from '@pulumi/pulumi';
-import { createCloudFrontDistribution, createOriginAccessIdentity } from './resources/CloudFront';
+import * as pulumi from "@pulumi/pulumi";
+import { createCacheInvalidation, createCloudFrontDistribution, createOriginAccessIdentity } from "./resources/CloudFront";
 import createLambdaAtEdgeFunction from "./resources/LambdaAtEdge";
 import createS3Bucket, { createS3BucketPermissions, uploadAssetsToBucket } from "./resources/S3Bucket";
-import { getSetup } from './utils/Setup';
+import { getSetup } from "./utils/Setup";
 
 const setup = getSetup();
 const originAccessIdentity = createOriginAccessIdentity(setup);
@@ -11,6 +11,7 @@ const edgeLambdaPackage = createLambdaAtEdgeFunction(setup, s3bucketSetup.name);
 createS3BucketPermissions(setup, s3bucketSetup.bucket, originAccessIdentity, edgeLambdaPackage.lambdaAtEdgeRole);
 const cloudFrontDistribution = createCloudFrontDistribution(setup, s3bucketSetup.bucket, originAccessIdentity, edgeLambdaPackage.lambdaAtEdgeFunction);
 uploadAssetsToBucket(s3bucketSetup.bucket);
+createCacheInvalidation(setup, cloudFrontDistribution);
 
 export const url = pulumi.interpolate`https://${cloudFrontDistribution.domainName}`;
 export const bucketName = s3bucketSetup.bucket.bucket;
--- infra/resources/CloudFront.ts
+++ infra/resources/CloudFront.ts
@@ -1,85 +1,95 @@
-import * as aws from '@pulumi/aws';
-import * as pulumi from '@pulumi/pulumi';
-import { ISetup } from '../utils/Setup';
+import * as aws from "@pulumi/aws";
+import { local } from "@pulumi/command";
+import * as pulumi from "@pulumi/pulumi";
+import { ISetup } from "../utils/Setup";
 
 export function createOriginAccessIdentity(setup: ISetup) {
-  const originAccessIdentityConfig = setup.getResourceConfig('OriginAccessIdentity');
-  return new aws.cloudfront.OriginAccessIdentity(
-    originAccessIdentityConfig.name,
-    {
-      comment: "Access Identity",
-    }
-  );
+  const originAccessIdentityConfig = setup.getResourceConfig("OriginAccessIdentity");
+  return new aws.cloudfront.OriginAccessIdentity(originAccessIdentityConfig.name, {
+    comment: "Access Identity",
+  });
 }
 
-export function createCloudFrontDistribution(setup: ISetup, bucket: aws.s3.Bucket, originAccessIdentity: aws.cloudfront.OriginAccessIdentity, lambdaAtEdgeFunction: aws.lambda.Function, ) {
-  const cloudFrontDistributionConfig = setup.getResourceConfig('CloudFrontDistribution');
-  
-  const cloudFrontDistribution = new aws.cloudfront.Distribution(
-    cloudFrontDistributionConfig.name,
-    {
-      origins: [
+export function createCloudFrontDistribution(
+  setup: ISetup,
+  bucket: aws.s3.Bucket,
+  originAccessIdentity: aws.cloudfront.OriginAccessIdentity,
+  lambdaAtEdgeFunction: aws.lambda.Function
+) {
+  const cloudFrontDistributionConfig = setup.getResourceConfig("CloudFrontDistribution");
+
+  const cloudFrontDistribution = new aws.cloudfront.Distribution(cloudFrontDistributionConfig.name, {
+    origins: [
+      {
+        domainName: bucket.bucketRegionalDomainName,
+        originId: bucket.arn,
+        s3OriginConfig: {
+          originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
+        },
+      },
+    ],
+    customErrorResponses: [],
+    defaultCacheBehavior: {
+      allowedMethods: ["GET", "HEAD", "OPTIONS"],
+      cachedMethods: ["GET", "HEAD"],
+      targetOriginId: bucket.arn,
+      viewerProtocolPolicy: "redirect-to-https",
+      forwardedValues: {
+        cookies: {
+          forward: "none",
+        },
+        queryString: false,
+      },
+      minTtl: 0,
+      defaultTtl: 2628000,
+      maxTtl: 31536000,
+      compress: true,
+      lambdaFunctionAssociations: [
         {
-          domainName: bucket.bucketRegionalDomainName,
-          originId: bucket.arn,
-          s3OriginConfig: {
-            originAccessIdentity:
-              originAccessIdentity.cloudfrontAccessIdentityPath,
-          },
+          eventType: "origin-request",
+          lambdaArn: pulumi.interpolate`${lambdaAtEdgeFunction.arn}:${lambdaAtEdgeFunction.version}`,
+          includeBody: true,
         },
       ],
-      customErrorResponses: [],
-      defaultCacheBehavior: {
-        allowedMethods: ['GET', 'HEAD', 'OPTIONS'],
-        cachedMethods: ['GET', 'HEAD'],
-        targetOriginId: bucket.arn,
-        viewerProtocolPolicy: 'redirect-to-https',
-        forwardedValues: {
-          cookies: {
-            forward: 'none',
-          },
-          queryString: false,
-        },
-        minTtl: 0,
-        defaultTtl: 2628000,
-        maxTtl: 31536000,
-        compress: true,
-        lambdaFunctionAssociations: [
-            {
-              eventType: 'origin-request',
-              lambdaArn: pulumi.interpolate `${lambdaAtEdgeFunction.arn}:${lambdaAtEdgeFunction.version}`,
-              includeBody: true,
-            },
-        ],
+    },
+    viewerCertificate: {
+      cloudfrontDefaultCertificate: true,
+    },
+    restrictions: {
+      geoRestriction: {
+        locations: [],
+        restrictionType: "none",
       },
-      viewerCertificate: {
-        cloudfrontDefaultCertificate: true,
-      },
-      restrictions: {
-        geoRestriction: {
-          locations: [],
-          restrictionType: 'none',
-        },
-      },
-      defaultRootObject: 'index.html',
-      httpVersion: 'http2',
-      isIpv6Enabled: true,
-      priceClass: 'PriceClass_All',
-      waitForDeployment: true,
-      enabled: true,
-      retainOnDelete: false,
-      tags: cloudFrontDistributionConfig.tags,
-    }
-  );
+    },
+    defaultRootObject: "index.html",
+    httpVersion: "http2",
+    isIpv6Enabled: true,
+    priceClass: "PriceClass_All",
+    waitForDeployment: true,
+    enabled: true,
+    retainOnDelete: false,
+    tags: cloudFrontDistributionConfig.tags,
+  });
 
   // Permissions for Lambda@Edge
-  const LambdaAtEdgePermissionConfig = setup.getResourceConfig('LambdaAtEdgePermission');
+  const LambdaAtEdgePermissionConfig = setup.getResourceConfig("LambdaAtEdgePermission");
   new aws.lambda.Permission(LambdaAtEdgePermissionConfig.name, {
-      action: 'lambda:GetFunction',
-      function: lambdaAtEdgeFunction.name,
-      principal: 'edgelambda.amazonaws.com',
-      sourceArn: cloudFrontDistribution.arn,
+    action: "lambda:GetFunction",
+    function: lambdaAtEdgeFunction.name,
+    principal: "edgelambda.amazonaws.com",
+    sourceArn: cloudFrontDistribution.arn,
   });
-  
+
   return cloudFrontDistribution;
+}
+
+export function createCacheInvalidation(setup: ISetup, distribution: aws.cloudfront.Distribution) {
+  const cacheInvalidationConfig = setup.getResourceConfig("CacheInvalidation");
+  new local.Command(
+    cacheInvalidationConfig.name,
+    {
+      create: pulumi.interpolate`aws cloudfront create-invalidation --distribution-id ${distribution.id} --paths resources/*`,
+    },
+    { deleteBeforeReplace: true }
+  );
 }
