0a01a424b8576cbca8a530d9d57902de2591eba5
Lassi Piironen <lsipii@kapsi.fi>
2023-03-28 15:10:53 +0300

    fix: municipalities bad parsing

--- src/resources/external/Municipalities.ts
+++ src/resources/external/Municipalities.ts
@@ -14,24 +14,17 @@ export default new Resource({
     uri: 'https://koodistot.suomi.fi/codelist-api/api/v1/coderegistries/jhs/codeschemes/kunta_1_20230101/?format=json&embedCodes=true&embedExtensions=true&embedMembers=true&expand=extension,member,codeScheme,code,memberValue,codeRegistry,organization,valueType,externalReference,propertyType&downloadFile=false&pretty',
     parsers: {
         async transform(koodistoResponse: any) {
-            return koodistoResponse['extensions'].reduce((municipalities: any, extension: any) => {
-                const members = extension['members']
-                    .filter((member: any) => typeof member['relatedMember'] !== 'undefined') // Filter out the regions
-                    .map((member: any) => {
+            return koodistoResponse['codes'].map((member: any) => {
+                return {
+                    Koodi: member['codeValue'],
+                    Selitteet: Object.entries(member['prefLabel']).map(([locale, name]) => {
                         return {
-                            Koodi: member['code']['codeValue'],
-                            Selitteet: Object.entries(member['code']['prefLabel']).map(([locale, name]) => {
-                                return {
-                                    Kielikoodi: locale,
-                                    Teksti: name,
-                                };
-                            }),
+                            Kielikoodi: locale,
+                            Teksti: name,
                         };
-                    });
-
-                municipalities.push(...members);
-                return municipalities;
-            }, []);
+                    }),
+                };
+            });
         },
         output(data: any) {
             return getOutput()<MunicipalityOutput[]>(data);
