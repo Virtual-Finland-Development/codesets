5172ed51c9b35ba0c1fb709b5dd71e312cbb39cb
Lassi Piironen <lsipii@kapsi.fi>
2023-03-29 14:46:51 +0300

    refactor: duplicate code toolified

--- src/resources/external/LevelsOfEducation.ts
+++ src/resources/external/LevelsOfEducation.ts
@@ -1,35 +1,6 @@
-import Resource from '../../utils/data/models/Resource';
-import { getOutput } from '../../utils/data/parsers';
+import SuomiFiKoodistotResource from '../../utils/data/models/SuomiFiKoodistotResource';
 
-interface LevelOfEducation {
-    codeValue: string;
-    order: number;
-    uri: string;
-    hierarchyLevel: number;
-    prefLabel: {
-        fi: string;
-        en: string;
-        se?: string;
-    };
-}
-
-export default new Resource({
+export default new SuomiFiKoodistotResource({
     name: 'LevelsOfEducation',
     uri: 'https://koodistot.suomi.fi/codelist-api/api/v1/coderegistries/dataecon/codeschemes/educationlevel/?format=json&embedCodes=true&embedExtensions=true&embedMembers=true&expand=extension,member,codeScheme,code,memberValue,codeRegistry,organization,valueType,externalReference,propertyType&downloadFile=false&pretty',
-    parsers: {
-        async transform(koodistoResponse: any) {
-            return koodistoResponse['codes'].map((permit: any) => {
-                return {
-                    codeValue: permit['codeValue'],
-                    order: permit['order'],
-                    uri: permit['uri'],
-                    hierarchyLevel: permit['hierarchyLevel'],
-                    prefLabel: permit['prefLabel'],
-                };
-            });
-        },
-        output(data: any) {
-            return getOutput()<LevelOfEducation[]>(data);
-        },
-    },
 });
