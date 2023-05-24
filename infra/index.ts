94cc43d78e54f29bf2367fbf58d59061f15c41c1
lassi.patanen <lassi.patanen@gofore.com>
2023-05-24 08:52:10 +0300

    Create S3 bucket for log files and configure cloud front distribution to send logs there

--- infra/index.ts
+++ infra/index.ts
@@ -5,19 +5,22 @@ import {
     createOriginAccessIdentity,
 } from './resources/CloudFront';
 import createLambdaAtEdgeFunction from './resources/LambdaAtEdge';
-import createS3Bucket, { createS3BucketPermissions, uploadAssetsToBucket } from './resources/S3Bucket';
-import { getSetup } from './utils/Setup';
+import createS3Bucket, {createS3BucketPermissions, uploadAssetsToBucket} from './resources/S3Bucket';
+import {getSetup} from './utils/Setup';
+import {createStandardLogsBucket} from "./resources/standardLogsBucket";
 
 const setup = getSetup();
 const originAccessIdentity = createOriginAccessIdentity(setup);
 const s3bucketSetup = createS3Bucket(setup);
 const edgeLambdaPackage = createLambdaAtEdgeFunction(setup, s3bucketSetup);
 createS3BucketPermissions(setup, s3bucketSetup.bucket, originAccessIdentity, edgeLambdaPackage.lambdaAtEdgeRole);
+const standardLogsBucket = createStandardLogsBucket(setup);
 const cloudFrontDistribution = createCloudFrontDistribution(
     setup,
     s3bucketSetup.bucket,
     originAccessIdentity,
-    edgeLambdaPackage.lambdaAtEdgeFunction
+    edgeLambdaPackage.lambdaAtEdgeFunction,
+    standardLogsBucket
 );
 uploadAssetsToBucket(s3bucketSetup.bucket);
 createCacheInvalidation(setup, cloudFrontDistribution);
---
+++ infra/resources/standardLogsBucket.ts
@@ -0,0 +1,18 @@
+import * as aws from "@pulumi/aws";
+import {Bucket} from "@pulumi/aws/s3";
+import {ISetup} from "../utils/Setup";
+
+export function createStandardLogsBucket(setup: ISetup): Bucket {
+    return new aws.s3.Bucket("standard-logs-bucket", {
+        lifecycleRules: [
+            {
+                enabled: true,
+                expiration: {
+                    days: 2
+                },
+                id: "standard-logs-expiration-rule"
+            }
+        ],
+        tags: setup.getResourceConfig('standard-logs-bucket').tags
+    });
+}
