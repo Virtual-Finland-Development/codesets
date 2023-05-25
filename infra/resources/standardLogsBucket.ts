af286031586242dc31a4e4578f939dfc312c1a25
lassi.patanen <lassi.patanen@gofore.com>
2023-05-25 08:41:48 +0300

    Enable ACL for logs bucket

--- infra/resources/standardLogsBucket.ts
+++ infra/resources/standardLogsBucket.ts
@@ -3,16 +3,17 @@ import {Bucket} from "@pulumi/aws/s3";
 import {ISetup} from "../utils/Setup";
 
 export function createStandardLogsBucket(setup: ISetup): Bucket {
-    return new aws.s3.Bucket("standard-logs-bucket", {
+    return new aws.s3.Bucket(`standard-logs-bucket-${setup.stage}`, {
         lifecycleRules: [
             {
                 enabled: true,
                 expiration: {
                     days: 2
                 },
-                id: "standard-logs-expiration-rule"
+                id: `standard-logs-expiration-rule-${setup.stage}`
             }
         ],
+        acl: 'private',
         tags: setup.getResourceConfig('standard-logs-bucket').tags
     });
 }
