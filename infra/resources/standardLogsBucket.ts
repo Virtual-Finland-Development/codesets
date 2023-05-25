cf7be66b36d221710dd9366fc8ba7779efa6ebab
lassi.patanen <lassi.patanen@gofore.com>
2023-05-25 08:54:08 +0300

    Configure bucket ownership controls to enable ACL for logs bucket

--- infra/resources/standardLogsBucket.ts
+++ infra/resources/standardLogsBucket.ts
@@ -3,7 +3,7 @@ import {Bucket} from "@pulumi/aws/s3";
 import {ISetup} from "../utils/Setup";
 
 export function createStandardLogsBucket(setup: ISetup): Bucket {
-    return new aws.s3.Bucket(`standard-logs-bucket-${setup.stage}`, {
+    const bucket = new aws.s3.Bucket(`standard-logs-bucket-${setup.stage}`, {
         lifecycleRules: [
             {
                 enabled: true,
@@ -16,4 +16,13 @@ export function createStandardLogsBucket(setup: ISetup): Bucket {
         acl: 'private',
         tags: setup.getResourceConfig('standard-logs-bucket').tags
     });
+    
+    const bucketOwnershipControls = new aws.s3.BucketOwnershipControls('controls', {
+       bucket: bucket.id,
+       rule: {
+           objectOwnership: "BucketOwnerPreferred"
+       } 
+    });
+    
+    return bucket;
 }
