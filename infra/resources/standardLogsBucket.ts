20953313695722fb9b34d646ca24352589281553
lassi.patanen <lassi.patanen@gofore.com>
2023-05-30 20:13:20 +0300

    Use eu-north-1 provider for standard logs bucket

--- infra/resources/standardLogsBucket.ts
+++ infra/resources/standardLogsBucket.ts
@@ -3,6 +3,7 @@ import {Bucket} from "@pulumi/aws/s3";
 import {ISetup} from "../utils/Setup";
 
 export function createStandardLogsBucket(setup: ISetup): Bucket {
+    const awsEuNorth1 = new aws.Provider("aws-eu-north-1", { region: "eu-north-1" });
     const bucket = new aws.s3.Bucket(`standard-logs-bucket-${setup.stage}`, {
         lifecycleRules: [
             {
@@ -15,6 +16,8 @@ export function createStandardLogsBucket(setup: ISetup): Bucket {
         ],
         acl: 'private',
         tags: setup.getResourceConfig('standard-logs-bucket').tags
+    }, {
+        provider: awsEuNorth1
     });
     
     const bucketOwnershipControls = new aws.s3.BucketOwnershipControls('controls', {
