f9aab61a643413431f3475b5a20efe15a27775e0
Lassi Piironen <lsipii@kapsi.fi>
2023-08-03 21:40:16 +0300

    build: upgrade to aws sdk v3

--- src/utils/lib/S3Bucket.ts
+++ src/utils/lib/S3Bucket.ts
@@ -1,16 +1,18 @@
-import aws from 'aws-sdk';
+import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { leftTrimSlash } from '../helpers';
 
 export async function storeToS3(bucketName: string, key: string, data: string, mime: string) {
     try {
-        const s3 = new aws.S3();
+        const s3Client = new S3Client({});
         const params = {
             Bucket: bucketName,
             Key: leftTrimSlash(key),
             Body: data,
             ContentType: mime,
         };
-        await s3.putObject(params).promise();
+        
+        await s3Client.send(new PutObjectCommand(params));
+
     } catch (error: any) {
         console.error(error?.message, error?.stack);
         throw new Error('An error occurred while storing to S3');
@@ -19,13 +21,15 @@ export async function storeToS3(bucketName: string, key: string, data: string, m
 
 export async function retrieveFromS3(bucketName: string, key: string): Promise<{ data: string; mime: string }> {
     try {
-        const s3 = new aws.S3();
+        const s3Client = new S3Client({});
         const params = {
             Bucket: bucketName,
             Key: leftTrimSlash(key),
         };
-        const data = await s3.getObject(params).promise();
 
+        const data  = await s3Client.send(new GetObjectCommand(params));
+
+        // Parse data body
         if (!data.Body) throw new Error('No data found in S3');
 
         return {
