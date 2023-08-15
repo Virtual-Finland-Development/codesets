ac1fdb60170ab7ad7c62dfdb9b91443712a2ddd2
Lassi Piironen <lsipii@kapsi.fi>
2023-08-15 22:38:21 +0300

    feat: handle exception logging better

--- src/app/RequestLogger.ts
+++ src/app/RequestLogger.ts
@@ -1,6 +1,5 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, CloudFrontHeaders, CloudFrontRequestEvent, CloudFrontRequestResult, CloudFrontResultResponse } from "aws-lambda";
 import { ValiError } from "valibot";
-import { cutTooLongString } from "../utils/helpers";
 
 export type LogPackage = {
     trace: {
@@ -17,8 +16,8 @@ export type LogPackage = {
     response: {
         statusCode: number;
         contentLength: number;
-        body?: string; // For error diagnostics
     };
+    errors: any[],
 };
 
 export type RequestLoggerSettings = {
@@ -30,7 +29,7 @@ export type RequestLoggerSettings = {
 export default class RequestLogger {
     private readonly request: APIGatewayProxyEventV2 | CloudFrontRequestEvent;
     private readonly configuration: RequestLoggerSettings;
-    private readonly errors: string[] = [];
+    private readonly errors: any[] = [];
 
     constructor(request: APIGatewayProxyEventV2 | CloudFrontRequestEvent, configuration?: RequestLoggerSettings) {
         this.request = request;
@@ -45,20 +44,17 @@ export default class RequestLogger {
         }
 
         if (typeof log.response.statusCode !== "number" || log.response.statusCode >= 400) {
-            log.response.body = cutTooLongString(log.response.body, 1000); // Limit logged body length to 1000 characters
-            console.error(JSON.stringify(log));
-            console.debug(JSON.stringify(this.errors));
+            console.error(JSON.stringify(log, null, 4));
         } else {
-            delete log.response.body; // Remove body from successful requests logs
             console.log(JSON.stringify(log));
         }
     }
 
-    public catchError(error: any) {
+    public catchError(error: Error | ValiError | any) {
         if (error instanceof ValiError)  {
             // Cleanup the vali error to make it more accessible: only show the first issue and remove the input
             this.errors.push(
-                JSON.stringify(error.issues.slice(0, 1).map(i => {
+                error.issues.slice(0, 1).map(i => {
                     return {
                     ...i,
                         path: i.path?.map(p => {
@@ -68,13 +64,19 @@ export default class RequestLogger {
                             }
                         })
                     }
-                }), null, 4)            
-            );
-        } else {
-            this.errors.push(JSON.stringify({
+                }           
+            ));
+        } else if (error instanceof Error)  {
+            this.errors.push({
                 message: error.message,
                 stack: error.stack,
-            }, null, 4));
+            });
+        } else {
+            try {
+                this.errors.push(JSON.stringify(error));
+            } catch (e) {
+                this.errors.push(error);
+            }
         }
     }
 
@@ -105,8 +107,8 @@ export default class RequestLogger {
             response: {
                 statusCode: statusCode || 500,
                 contentLength: body ? body.length : 0,
-                body: body,
             },
+            errors: this.errors,
         };
     }
 
@@ -129,6 +131,7 @@ export default class RequestLogger {
                 query: eventRequest.querystring,
             },
             response: eventResponse,
+            errors: this.errors,
         };
     }
 
@@ -144,18 +147,15 @@ export default class RequestLogger {
         const parsedResponse: LogPackage["response"] = {
             statusCode: 500,
             contentLength: 0,
-            body: undefined,
         };
 
         if (typeof response === "object" && response !== null) {
             if (typeof (response as CloudFrontResultResponse).status === "string") {
                 parsedResponse.statusCode = parseInt((response as CloudFrontResultResponse).status);
                 parsedResponse.contentLength = (response as CloudFrontResultResponse).body?.length || 0;
-                parsedResponse.body = (response as CloudFrontResultResponse).body;
             } else if (typeof response.body === "object" && response.body !== null && typeof response.body.data === "string") {
                 parsedResponse.statusCode = 200; // Pass-through response code
                 parsedResponse.contentLength = response.body.data.length;
-                parsedResponse.body = response.body.data;
             }
         }
         
