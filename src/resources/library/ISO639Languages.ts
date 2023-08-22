9f0797a665441e1270a8d2b72ed4859f9d9adf8a
Lauri Saarela <lauri.saarela@gofore.com>
2023-08-22 11:20:11 +0300

    match definition codes

--- src/resources/library/ISO639Languages.ts
+++ src/resources/library/ISO639Languages.ts
@@ -1,12 +1,17 @@
 const { iso_639_1, iso_639_2 } = require('iso-639');
 
-export default Object.values(iso_639_1).map((language: any) => {
-    return {
-        id: language['639-2'],
-        displayName: language.name,
-        englishName: iso_639_2[language['639-2']]?.en[0] || language.name,
-        nativeName: language.nativeName,
-        twoLetterISOLanguageName: language['639-1'],
-        threeLetterISOLanguageName: language['639-2'],
-    };
-});
+// ISO codes not defined in JobApplicantProfile definition
+const EXCLUDE_LANG_CODES = ['bh', 'zu'];
+
+export default Object.values(iso_639_1)
+    .filter((language: any) => !EXCLUDE_LANG_CODES.includes(language['639-1']))
+    .map((language: any) => {
+        return {
+            id: language['639-2'],
+            displayName: language.name,
+            englishName: iso_639_2[language['639-2']]?.en[0] || language.name,
+            nativeName: language.nativeName,
+            twoLetterISOLanguageName: language['639-1'],
+            threeLetterISOLanguageName: language['639-2'],
+        };
+    });
