45bfeebdd5bd86cc8a64946d68bc11836903ec89
Lauri Saarela <lauri.saarela@gofore.com>
2023-08-22 11:23:33 +0300

    link to definiotion in comments

--- src/resources/library/ISO639Languages.ts
+++ src/resources/library/ISO639Languages.ts
@@ -1,6 +1,7 @@
 const { iso_639_1, iso_639_2 } = require('iso-639');
 
 // ISO codes not defined in JobApplicantProfile definition
+// https://github.com/Virtual-Finland/definitions/blob/main/DataProducts/draft/Person/JobApplicantProfile/Write.json#L3880C10-L3880C10
 const EXCLUDE_LANG_CODES = ['bh', 'zu'];
 
 export default Object.values(iso_639_1)
