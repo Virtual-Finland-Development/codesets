dce191e28d821c3904b5122e8782b99cdbd30e78
Lauri Saarela <lauri.saarela@gofore.com>
2023-09-18 16:07:10 +0300

    cloudwatch lambda handler - handle events as documented in AWS for subscription filters, dunno if correct way

--- src/cloudwatch-alarm-subscriber.ts
+++ src/cloudwatch-alarm-subscriber.ts
@@ -1,9 +1,46 @@
-// import { CloudWatchLogsEvent } from 'aws-lambda';
+import { CloudWatchLogsEvent } from 'aws-lambda';
 import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
+import { gunzip } from 'zlib';
 
 const snsClient = new SNSClient({ region: 'eu-north-1' });
 
-export const handler = async (event: any) => {
+// https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/SubscriptionFilters.html#LambdaFunctionExample
+export const handler = async (event: CloudWatchLogsEvent) => {
+    try {
+        const snsTopicArn = process.env.SNS_TOPIC_ARN || undefined;
+
+        if (!snsTopicArn) {
+            throw new Error('SNS topic arn could not be read from env.');
+        }
+
+        const payload = Buffer.from(event.awslogs.data, 'base64');
+
+        gunzip(payload, async (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            const data = JSON.parse(result.toString());
+            console.log('Event Data:', JSON.stringify(data, null, 2));
+
+            const message = 'Test 123 123.';
+
+            const publishCommand = new PublishCommand({
+                TopicArn: snsTopicArn,
+                Message: message,
+                Subject: `CloudWatch Alarm: ${message}`,
+            });
+
+            await snsClient.send(publishCommand);
+            console.log(`Published message to SNS: ${message}`);
+        });
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
+
+/* export const handler = async (event: any) => {
     try {
         console.log(event);
         const snsTopicArn = process.env.SNS_TOPIC_ARN || undefined;
@@ -14,11 +51,11 @@ export const handler = async (event: any) => {
 
         // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cloudwatch-alarm.html
         // ????
-        /* const alarmName: string = event.detail['alarmName'];
+        const alarmName: string = event.detail['alarmName'];
         const alarmDescription: string = event.detail['alarmDescription'];
-        const alarmStateValue: string = event.detail['newStateValue']; */
+        const alarmStateValue: string = event.detail['newStateValue'];
 
-        /*  const message = `Alarm "${alarmName}" has entered state "${alarmStateValue}" - ${alarmDescription}`; */
+         const message = `Alarm "${alarmName}" has entered state "${alarmStateValue}" - ${alarmDescription}`;
         const message = 'Test 123 123.';
 
         const publishCommand = new PublishCommand({
@@ -34,3 +71,4 @@ export const handler = async (event: any) => {
         throw error;
     }
 };
+ */
